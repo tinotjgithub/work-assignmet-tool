f4dc0aacf7ecd1ea0292e9c388e5b89070734751
tinotgithub <U87610@ust-global.com>
2019-11-22 15:20:18 +0530

    Committing changes

--- src/app/services/auth/auth.interceptor.ts
+++ src/app/services/auth/auth.interceptor.ts
@@ -10,7 +10,7 @@ export class AuthInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         const authToken = this.authService.getAuthToken();
         const authRequest = req.clone({
-            headers: req.headers.set("Authorization", "Bearer " + authToken)
+            // headers: req.headers.set("Authorization", "Bearer " + authToken)
         });
         return next.handle(authRequest);
     }
