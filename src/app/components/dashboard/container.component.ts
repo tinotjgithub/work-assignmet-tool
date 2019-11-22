7084acba205f8ca5b16d7a5fd0dfa14ed93e6046
tinotgithub <U87610@ust-global.com>
2019-11-22 17:57:19 +0530

    Committing Dash board Data

--- src/app/components/dashboard/container.component.ts
+++ src/app/components/dashboard/container.component.ts
@@ -10,6 +10,20 @@ import { Router } from "@angular/router";
 export class ContainerComponent implements OnInit {
   constructor(private router: Router) {}
 
+  private setClassorNav() {
+    $(document).ready(function() {
+      $(".activelink").click(function(e) {
+        $(".activelink").removeClass("active");
+        var $this = $(this);
+        if (!$this.hasClass("active")) {
+          $this.addClass("active");
+        }
+        e.preventDefault();
+      });
+    });
+  }
+
+  id = "USER";
   status: boolean = false;
   myData = [
     ["Active", 100],
@@ -69,18 +83,49 @@ export class ContainerComponent implements OnInit {
   widthlineIntake = 550;
   heightlineIntake = 400;
 
+  // Progress Chart
+
+  lineTypeProgress = "LineChart";
+
+  titlelineProgress = "";
+  typelineProgress = "LineChart";
+  datalineProgress = [
+    ["1 hr", 20, 25],
+    ["2 hr", 20, 18],
+    ["3  hr", 20, 23],
+    ["4 hr", 20, 27],
+    ["5 hr", 20, 16],
+    ["6 hr", 20, 19],
+    ["7 hr", 20, 30],
+    ["8 hr", 20, 28],
+    ["9 hr", 20, 20]
+  ];
+  columnNameslineProgress = ["Week", "Target", "Count"];
+  optionslineProgress = {
+    hAxis: {
+      title: "Target"
+    },
+    vAxis: {
+      title: "Count"
+    }
+  };
+  widthlineProgress = 550;
+  heightlineProgress = 400;
+
   clickEvent() {
     this.status = !this.status;
   }
 
+  setComponentId(id) {
+    this.id = id;
+  }
+
   ngOnInit() {
+    this.setClassorNav();
     let that = this;
     let chart = new CanvasJS.Chart("chartContainer", {
       animationEnabled: true,
       exportEnabled: true,
-      title: {
-        text: "Aging Dollar Impact"
-      },
       data: [
         {
           type: "column",
@@ -95,9 +140,9 @@ export class ContainerComponent implements OnInit {
           },
           dataPoints: [
             { y: 200, label: "0$ - 200$" },
-            { y: 350, label: "$100 - $1000" },
-            { y: 175, label: "$1000- $20,000" },
-            { y: 10, label: "$20,000 - Above" }
+            { y: 160, label: "$100 - $1000" },
+            { y: 135, label: "$1000- $20,000" },
+            { y: 27, label: "$20,000 - Above" }
           ]
         }
       ]
@@ -106,9 +151,6 @@ export class ContainerComponent implements OnInit {
     let chart1 = new CanvasJS.Chart("chartContainer1", {
       animationEnabled: true,
       exportEnabled: true,
-      title: {
-        text: "SLA Risk - Auth Count"
-      },
       data: [
         {
           type: "column",
@@ -147,10 +189,12 @@ export class ContainerComponent implements OnInit {
             });
           },
           dataPoints: [
-            { y: 200, label: "0$ - 200$" },
-            { y: 350, label: "$100 - $1000" },
-            { y: 175, label: "$1000- $20,000" },
-            { y: 10, label: "$20,000 - Above" }
+            { y: 125, label: "0-10" },
+            { y: 76, label: "0-20" },
+            { y: 8, label: "20-30" },
+            { y: 6, label: "30-60" },
+            { y: 2, label: "60-90" },
+            { y: 0, label: "90 Above" }
           ]
         }
       ]
@@ -172,10 +216,10 @@ export class ContainerComponent implements OnInit {
             });
           },
           dataPoints: [
-            { y: 200, label: "0$ - 200$" },
-            { y: 350, label: "$100 - $1000" },
-            { y: 175, label: "$1000- $20,000" },
-            { y: 10, label: "$20,000 - Above" }
+            { y: 5, label: "Auth" },
+            { y: 15, label: "Dup Logic" },
+            { y: 7, label: "NPP WB" },
+            { y: 15, label: "COP WB" }
           ]
         }
       ]
